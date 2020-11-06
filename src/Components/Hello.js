1ab76ad490d1d87f7a849fb22ee37d4b11bb0bc3
Adwoa-Precious <favourdorothy2017@gmail.com>
2020-11-06 17:59:34 +0000

    Create Hello.js

---
+++ src/Components/Hello.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+// import { Navbar,Nav,NavDropdown,Form,FormControl,Button } from 'react-bootstrap'
+
+
+class Hello extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Welcome</h1>
+               
+            </div>
+        )
+    }
+}
+
+export default Hello
